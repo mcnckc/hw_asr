f55589a999863c4372dfbcb0af1d8c7d4557c412
mcnckc <s20b1_gusev@179.ru>
2023-10-26 17:49:18 +0300

    pyctc beam search

--- hw_asr/metric/__init__.py
+++ hw_asr/metric/__init__.py
@@ -2,10 +2,14 @@ from hw_asr.metric.cer_metric import ArgmaxCERMetric
 from hw_asr.metric.wer_metric import ArgmaxWERMetric
 from hw_asr.metric.bs_wer_metric import BeamSearchWERMetric
 from hw_asr.metric.bs_cer_metric import BeamSearchCERMetric
+from hw_asr.metric.pyctc_bs_wer_metric import PyCTCBeamSearchWERMetric
+from hw_asr.metric.pyctc_bs_cer_metric import PyCTCBeamSearchCERMetric
 
 __all__ = [
     "ArgmaxWERMetric",
     "ArgmaxCERMetric",
     "BeamSearchWERMetric",
-    "BeamSearchCERMetric"
+    "BeamSearchCERMetric",
+    "PyCTCBeamSearchWERMetric",
+    "PyCTCBeamSearchCERMetric"
 ]
---
+++ hw_asr/metric/pyctc_bs_cer_metric.py
@@ -0,0 +1,32 @@
+from typing import List
+
+import torch
+from torch import Tensor
+import multiprocessing
+from hw_asr.base.base_metric import BaseMetric
+from hw_asr.base.base_text_encoder import BaseTextEncoder
+from hw_asr.metric.utils import calc_cer
+from pyctcdecode import build_ctcdecoder
+from hw_asr.utils import ROOT_PATH
+
+class PyCTCBeamSearchCERMetric(BaseMetric):
+    def __init__(self, text_encoder: BaseTextEncoder, model_path, beam_size=100,*args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.text_encoder = text_encoder
+        self.beam_size = beam_size
+        self.decoder = build_ctcdecoder(
+            [text_encoder.EMPTY_TOK] + list(text_encoder.alphabet),
+            kenlm_model_path=ROOT_PATH / model_path,  # either .arpa or .bin file
+            alpha=0.5,  # tuned on a val set
+            beta=1.0,  # tuned on a val set
+        )
+
+    def __call__(self, log_probs: Tensor, log_probs_length: Tensor, text: List[str], **kwargs):
+        cers = []
+        lengths = log_probs_length.detach().numpy()
+        with multiprocessing.get_context("fork").Pool() as pool:
+            pred_list = self.decoder.decode_batch(pool, log_probs, beam_width=self.beam_size)
+        for pred_text, target_text in zip(pred_list, text):
+            target_text = BaseTextEncoder.normalize_text(target_text)
+            cers.append(calc_cer(target_text, pred_text))
+        return sum(cers) / len(cers)
